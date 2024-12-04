1ea6924c2d0c8d2e84fb93897e8cb88346b8fcda
simonsites <si.studios13@gmail.com>
2024-12-04 18:44:19 -0500

    Progress

---
+++ src/nav.js
@@ -0,0 +1,30 @@
+window.onscroll = function() {scrollFunction()}
+function scrollFunction() {
+    if (window.innerWidth < 600) {
+        if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+        
+            
+            document.getElementById("navbar").classList.add("box")
+
+          } else {
+            document.getElementById("navbar").classList.remove("box")
+           
+     
+          }
+      
+    }
+ 
+    else {
+        if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+        
+            
+            document.getElementById("navbar").classList.add("box")
+            
+     
+          } else {
+            document.getElementById("navbar").classList.remove("box")
+          
+     
+          }
+    }
+  }
---
+++ tailwind.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        navy: "#2c3e50", /* Soft Navy */
+        sand: "#f4e3c1", /* Warm Sand */
+    
+        /* Accent Colors */
+        coral: "#e74c3c", /* Deep Coral */
+        green: "#8eac50", /* Sage Green */
+    
+        /* Neutral Colors */
+        light: "#ffffff", /* Pure White */
+        dark: "#202020" /* Charcoal Gray */
+
+      }
+    }
+  }
+}
