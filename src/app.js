a8f5dd9fe4007993c36345d5ac4fc311e50c4264
simonsites <si.studios13@gmail.com>
2024-12-05 17:40:14 -0500

    progress

--- src/app.js
+++ src/app.js
@@ -1,4 +1,33 @@
+window.onscroll = function() {scrollFunction()}
+function scrollFunction() {
+    if (window.innerWidth < 600) {
+        if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+        
+            
+            document.getElementById("navbar").classList.add("box")
 
+          } else {
+            document.getElementById("navbar").classList.remove("box")
+           
+     
+          }
+      
+    }
+ 
+    else {
+        if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+        
+            
+            document.getElementById("navbar").classList.add("box")
+            
+     
+          } else {
+            document.getElementById("navbar").classList.remove("box")
+          
+     
+          }
+    }
+  }
 
 
 let video = document.getElementById('hero');
