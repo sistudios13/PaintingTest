b9c8a095d62cb45c4f3662bd5c05371a4cfb6b55
Westr <si.studios13@gmail.com>
2024-12-08 19:37:20 -0500

    f

--- src/app.js
+++ src/app.js
@@ -1,43 +1,44 @@
-window.onscroll = function() {scrollFunction()}
+// Nav
 
-let scrollHeight = 90
+window.onscroll = function () {
+	scrollFunction();
+};
+
+let scrollHeight = 90;
+let th = 0.5
 
 function scrollFunction() {
-    if (window.innerWidth < 600) {
-        if ( document.documentElement.scrollTop > scrollHeight) {
-        
-            
-            document.getElementById("navbar").classList.add("box")
-
-          } else {
-            document.getElementById("navbar").classList.remove("box")
-           
-     
-          }
-      
-    }
- 
-    else {
-        if (document.body.scrollTop > scrollHeight || document.documentElement.scrollTop > scrollHeight) {
-        
-            
-            document.getElementById("navbar").classList.add("box")
-            
-     
-          } else {
-            document.getElementById("navbar").classList.remove("box")
-          
-     
-          }
-    }
-  }
-
-
-let video = document.getElementById('hero');
-
-function playVid() {
-    video.play()
+	if (window.innerWidth < 600) {
+    th = 0.7
+		if (document.documentElement.scrollTop > scrollHeight) {
+			document.getElementById("navbar").classList.add("box");
+		} else {
+			document.getElementById("navbar").classList.remove("box");
+		}
+	} else {
+    th = 0.5
+		if (
+			document.body.scrollTop > scrollHeight ||
+			document.documentElement.scrollTop > scrollHeight
+		) {
+			document.getElementById("navbar").classList.add("box");
+		} else {
+			document.getElementById("navbar").classList.remove("box");
+		}
+	}
 }
-function pauseVid() {
-    video.pause()
-}
+
+// Anims
+
+const fadeobserver = new IntersectionObserver((entries) => {
+  
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add("fadeupanim");
+		}
+	});
+}, { threshold : th });
+
+const fadeElements = document.querySelectorAll(".fadeup");
+
+fadeElements.forEach((el) => fadeobserver.observe(el));
